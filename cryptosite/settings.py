1e896046d51a553f23d79d68622a404207d3e037
kspalek <spal2233@gmail.com>
2022-10-10 18:22:37 +0200

    static root

--- cryptosite/settings.py
+++ cryptosite/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = "/home/kspalek/kspalek.pythonanywhere.com/static/"
+STATIC_ROOT = "/cryptoapp/static/cryptoapp/css"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
