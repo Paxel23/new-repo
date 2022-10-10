14e01a69a86cc199f325dc697874fb4982b2b5fa
kspalek <spal2233@gmail.com>
2022-10-10 17:04:05 +0200

    sss

--- cryptoapp/urls.py
+++ cryptoapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -34,3 +37,5 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
